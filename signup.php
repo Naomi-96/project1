dfcc33bcacf8d6c4b7d7a68a0f64328aa394aaa4
naomi <naomi.giersthove@student.rocva.nl>
2020-09-09 14:32:51 +0200

    .

--- signup.php
+++ signup.php
@@ -3,17 +3,22 @@
  ?>
 
 <html>
+  <head>
 
-  <form action="signup.php" method="get">
-    <input type="text" name="firstname" placeholder="Type your first name" required/><br>
-    <input type="text" name="insertion" placeholder="Insertion" /<br>
-    <input type="text" name="lastname" placeholder="Type your last name" required/<br>
-    <input type="email" name="email" placeholder="Type your email" required/<br>
-    <input type="text" name="username" placeholder="Type your username" required/<br>
-    <input type="password" name="password" placeholder="Type your password" required/<br>
-    <input type="password" name="repeatpassword" placeholder="Type your password again" required/<br>
-    <input type="submit" value="signup" /><br>
-    <a href="index.php">Login</a>
- </form>
+  </head>
+
+  <body>
+    <form action="signup.php" method="get">
+      <input type="text" name="firstname" placeholder="Type your first name" required/><br>
+      <input type="text" name="insertion" placeholder="Insertion" /<br>
+      <input type="text" name="lastname" placeholder="Type your last name" required/<br>
+      <input type="email" name="email" placeholder="Type your email" required/<br>
+      <input type="text" name="username" placeholder="Type your username" required/<br>
+      <input type="password" name="password" placeholder="Type your password" required/<br>
+      <input type="password" name="repeatpassword" placeholder="Type your password again" required/<br>
+      <input type="submit" value="signup" /><br>
+      <a href="index.php">Login</a>
+    </form>
+  </body>
 
 </html>
