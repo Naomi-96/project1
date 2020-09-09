506d38cc551d9d7cfbbc879715c1ea0e730ce0a3
Naomi-96 <70630946+Naomi-96@users.noreply.github.com>
2020-09-09 13:39:54 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+ ?>
+
+
+<html>
+  <head>
+
+  </head>
+
+  <form action="index.php" method="get">
+    <input type="text" name="username" placeholder="Username" required /><br>
+    <input type="password" name="password" placeholder="Password" required /><br>
+    <input type="submit" /><br>
+    <a href="signup.php" Signup </a><br>
+    <a href="lostpwd.php" Forgot your password? </a><br>
+  </form>
+
+</html>
---
+++ lostpsw.php
@@ -0,0 +1,16 @@
+<?php
+
+?>
+
+<html>
+  <head>
+  </head>
+
+  <body>
+    <form action="lostpwd.php" method="get">
+      <input type="email" placeholder="Type your email" required /><br>
+      <input type="submit" value="submit"/>
+    </form>
+  </body>
+
+</html>
